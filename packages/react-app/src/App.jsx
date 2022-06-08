cad3ab7cd39b462134bf197fb057cdf1bb938eed
Daniele Salatti <me@danielesalatti.com>
2022-06-08 15:30:13 +0200

    Remove bottom indicators

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -304,31 +304,6 @@ function App(props) {
 
       <ThemeSwitch />
 
-      {/* 🗺 Extra UI like gas price, eth price, faucet, and support: */}
-      <div style={{ position: "fixed", textAlign: "left", left: 0, bottom: 20, padding: 10 }}>
-        <Row align="middle" gutter={[4, 4]}>
-          <Col span={8}>
-            <Ramp price={price} address={address} networks={NETWORKS} />
-          </Col>
-
-          <Col span={8} style={{ textAlign: "center", opacity: 0.8 }}>
-            <GasGauge gasPrice={gasPrice} />
-          </Col>
-        </Row>
-
-        <Row align="middle" gutter={[4, 4]}>
-          <Col span={24}>
-            {
-              /*  if the local provider has a signer, let's show the faucet:  */
-              faucetAvailable ? (
-                <Faucet localProvider={localProvider} price={price} ensProvider={mainnetProvider} />
-              ) : (
-                ""
-              )
-            }
-          </Col>
-        </Row>
-      </div>
       <Footer />
     </div>
   );
